47f909739108e1ce59f82dc4998942ad597adc82
Meenakshi-Anand <abhi@codeanand.com>
2018-09-27 13:11:50 -0700

    create access to calendar in calendar_controller

--- meetingnotes/config/routes.rb
+++ meetingnotes/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-
+  get '/calendars', to: 'example#calendars', as: 'calendars'
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
