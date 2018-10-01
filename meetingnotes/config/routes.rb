a5a2d905c79ac0bad96485958946463f17ae47fd
Meenakshi-Anand <abhi@codeanand.com>
2018-10-01 13:10:26 -0700

    fetch all calendar events

--- meetingnotes/config/routes.rb
+++ meetingnotes/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'signout', to: 'sessions#destroy', as: 'signout'
   get '/calendars', to: 'calendar#calendars', as: 'calendars'
   get '/redirect', to: 'calendar#redirect', as: 'redirect'
-  get '/callback', to: 'calendar#callback', as: 'callback' 
+  get '/callback', to: 'calendar#callback', as: 'callback'
+  get '/events/:calendar_id', to: 'calendar#events', as: 'events', calendar_id: /[^\/]+/
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
