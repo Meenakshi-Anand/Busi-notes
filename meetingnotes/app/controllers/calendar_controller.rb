0c8a2622892c2236e0a97899054b3f1bb13f1340
Meenakshi-Anand <abhi@codeanand.com>
2018-10-01 12:54:18 -0700

    allow calendar acces

--- meetingnotes/app/controllers/calendar_controller.rb
+++ meetingnotes/app/controllers/calendar_controller.rb
@@ -6,6 +6,17 @@ class CalendarController < ApplicationController
     redirect_to client.authorization_uri.to_s
   end
 
+  def callback
+    client = Signet::OAuth2::Client.new(client_options)
+    client.code = params[:code]
+
+    response = client.fetch_access_token!
+
+    session[:authorization] = response
+
+    redirect_to calendars_url
+  end
+
   private
 
   def client_options
@@ -14,7 +25,7 @@ class CalendarController < ApplicationController
       client_secret: Rails.application.secrets.google_client_secret,
       authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-      scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
       redirect_uri: callback_url
     }
   end
--- meetingnotes/config/routes.rb
+++ meetingnotes/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get '/calendars', to: 'example#calendars', as: 'calendars'
+  get '/calendars', to: 'calendar#calendars', as: 'calendars'
+  get '/redirect', to: 'calendar#redirect', as: 'redirect'
+  get '/callback', to: 'calendar#callback', as: 'callback'
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
