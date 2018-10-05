53e66358e88e8f33ba0b13b01b14b280a67a2a16
Meenakshi-Anand <abhi@codeanand.com>
2018-10-04 18:39:21 -0700

    add email to users table

--- meetingnotes/db/schema.rb
+++ meetingnotes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_174621) do
+ActiveRecord::Schema.define(version: 2018_10_05_013646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_174621) do
     t.string "uid"
     t.string "refresh_token"
     t.string "code"
+    t.string "email"
   end
 
 end
